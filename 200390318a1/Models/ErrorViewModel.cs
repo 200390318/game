c03af3b98fc77c69bd9bafbe3996bd38c83cdead
200390318 <46460554+200390318@users.noreply.github.com>
2019-02-03 22:20:25 -0500

    Add project files.

---
+++ 200390318a1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _200390318a1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
